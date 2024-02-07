839f034f6299e355234ec33147b425a2ce5e48b1
Pelle Traalum Okkenhaug <pelle.okkenhaug@gmail.com>
2024-02-07 02:21:38 +0100

    boxes and grid for destinations, home page first draft #1

--- destinationdino/frontend/destinationdino/src/pages/Home.tsx
+++ destinationdino/frontend/destinationdino/src/pages/Home.tsx
@@ -1,3 +1,20 @@
+import DestinationGrid from "../components/DestinationGrid";
+import destinationImage from "../assets/bilde1.jpg";
+import destinationImage2 from "../assets/bilde2.jpg";
+
+// Example destinations
+const destinations = [
+  { image: destinationImage, name: "Destination 1", rating: 4.5 },
+  { image: destinationImage2, name: "Destination 2", rating: 4.7 },
+];
+
 export default function Home() {
-    return <h1>Home</h1>
+  return (
+    <>
+      <h1>Home</h1>
+      <div>
+        <DestinationGrid destinations={destinations} />
+      </div>
+    </>
+  );
 }
