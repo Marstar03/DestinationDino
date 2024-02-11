ca0c5211dc3bcb04b3c01bb29dff2e771a1d8072
Pelle Traalum Okkenhaug <pelle.okkenhaug@gmail.com>
2024-02-11 15:49:44 +0100

    Front page nicer #1 Co-authored-by: Karoline Torø <karoltor@stud.ntnu.no>

--- destinationdino/frontend/destinationdino/src/pages/Home.tsx
+++ destinationdino/frontend/destinationdino/src/pages/Home.tsx
@@ -6,12 +6,21 @@ import destinationImage2 from "../assets/bilde2.jpg";
 const destinations = [
   { image: destinationImage, name: "Destination 1", rating: 4.5 },
   { image: destinationImage2, name: "Destination 2", rating: 4.7 },
+  { image: destinationImage2, name: "Destination 2", rating: 4.7 },
+  { image: destinationImage2, name: "Destination 2", rating: 4.7 },
+  { image: destinationImage2, name: "Destination 2", rating: 4.7 },
+  { image: destinationImage2, name: "Destination 2", rating: 4.7 },
+  { image: destinationImage2, name: "Destination 2", rating: 4.7 },
+  { image: destinationImage2, name: "Destination 2", rating: 4.7 },
+  { image: destinationImage2, name: "Destination 2", rating: 4.7 },
+  { image: destinationImage2, name: "Destination 2", rating: 4.7 },
+  { image: destinationImage2, name: "Destination 2", rating: 4.7 },
+  { image: destinationImage2, name: "Destination 2", rating: 4.7 },
 ];
 
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
       <div>
         <DestinationGrid destinations={destinations} />
       </div>
