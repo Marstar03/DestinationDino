071a1dc6417aebefc9652c5010e2c94de8340e9c
Pelle Traalum Okkenhaug <pelle.okkenhaug@gmail.com>
2024-02-11 22:02:10 +0100

    Sign in page works properly #7

--- destinationdino/frontend/destinationdino/src/App.tsx
+++ destinationdino/frontend/destinationdino/src/App.tsx
@@ -1,23 +1,25 @@
-import './App.css'
-import Navbar from './Navbar';
-import About from './pages/About';
-import Home from './pages/Home';
-import Profile from './pages/Profile';
-import { Route, Routes } from 'react-router-dom'
+import "./App.css";
+import Navbar from "./Navbar";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignInSide from "./pages/SignInSide";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-  <>
-    <Navbar />
-    <div className='container'>
+    <>
+      <Navbar />
+      <div className="container">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/about" element={<About />} />
+          <Route path="/SigninSide" element={<SignInSide />} />
         </Routes>
-    </div>
-  </>
-  )
-};
+      </div>
+    </>
+  );
+}
 
 export default App;
