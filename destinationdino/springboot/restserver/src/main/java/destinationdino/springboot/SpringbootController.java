965094a2497d77f4ca8cb10d104993dc18bf7132
kjaustgu <kjaustgu@stud.ntnu.no>
2024-02-15 16:03:31 +0100

    added cross origin #5 Co-author: Sander, Markus

--- destinationdino/springboot/restserver/src/main/java/destinationdino/springboot/SpringbootController.java
+++ destinationdino/springboot/restserver/src/main/java/destinationdino/springboot/SpringbootController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("http://localhost:5173")
 public class SpringbootController {
     
     private final DestinationService destinationService;
