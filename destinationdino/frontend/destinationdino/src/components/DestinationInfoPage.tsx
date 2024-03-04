d7890c09f8c2b69655c154b3e07babb7e10759a7
Sang <108661792+SangMunnKim@users.noreply.github.com>
2024-03-04 16:14:40 +0100

    Fix errors
    Lagde egen CSS copmonent for DestinationInfoPage
    Justerte på getRequest for å fikse på feilmedlinger som var
    fobundet med å hente data fra backend

--- destinationdino/frontend/destinationdino/src/components/DestinationInfoPage.tsx
+++ destinationdino/frontend/destinationdino/src/components/DestinationInfoPage.tsx
@@ -1,131 +1,65 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import BoxForDestinationInfo from './BoxForDestinationInfo';
 import { getRequest } from '../httpMethods/getRequest';
-import styled from 'styled-components';
-import { createGlobalStyle } from 'styled-components';
 import { useParams } from 'react-router-dom';
 import CustomCheckbox from './CheckBox';
+import {
+  GlobalStyle,
+  GridContainer,
+  Picture,
+  Heading,
+  Title,
+  Score,
+  Rating,
+  Check,
+  Description,
+  Attraction
+} from './DestinationInfoPageCSS';
 
 const DestinationInfoPage: React.FC = () => {
   
   const { name } = useParams();
-  const apiUrl = `http://localhost:8080/destinationInfo?id=${encodeURIComponent(name)}`;
-  const { data, loading, error } = getRequest(apiUrl);
-  console.log(name);
+  const apiUrl = `http://localhost:8080/destinationInfo?id=${encodeURIComponent(name as string)}`;
   
-  // Loading state
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  // Error state
-  if (error) {
-    return <div>Error!</div>;
-  }
-
-  //Front-end
-  const GlobalStyle = createGlobalStyle`
-    body {
-      color: black;
+  const { data, loading, error } = getRequest(apiUrl);
+  if (!data) {
+      // Loading state
+    if (loading) {
+      return <div>Loading...</div>;
     }
-  `;
 
-  const GridContainer = styled.div`
-    display: grid;
-    grid-template-columns: 100vh;
-    grid-template-rows: auto auto auto;
-    grid-gap: 10px;
-  `;
+    // Error state
+    if (error) {
+      return <div>Error!</div>;
+    }
+  }
 
-  const Picture = styled.div`
-    background: url(${props => props.pictureUrl}) no-repeat center center;
-    background-size: cover;
-    height: 400px;
-  `;
-
-  const Heading = styled.div`
-    grid-column: 1 / -1;
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-gap: 10px;
-  `;
-
-  const Title = styled.div`
-    background-color: #F2F2F2;
-    padding: 15px;
-    text-align: center;
-  `;
-
-  const Score = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 2fr;
-    grid-gap: 10px;
-  `;
-
-  const Rating = styled.div`
-  background-color: lightblue;
-  margin: 0;
-  padding:0;
-  `;
-
-  const Check = styled.div`
-  background-color: #F2F2F2;
-  display:flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 20px;
-  font-style: bold;
-  `;
-
-  const Description = styled.div`
-    background-color: #F2F2F2;
-    padding: 15px;
-    text-align: center;
-  `;
-
-  const Attraction = styled.div`
-    background-color: #E9E9E9;
-    padding: 15px;
-    text-align: center;
-  `;
 
   return (
     <div>
       <GlobalStyle />
       <GridContainer>
-        {data && <Picture pictureUrl={data.picture} />}
+        {data && <Picture $picture_url={data.picture} />}
         <Heading>
           <Title>
-            {data && (
-            <BoxForDestinationInfo title="Destination" content={`${data.name}, ${data.country}`} />
-            )}
+            {data && <BoxForDestinationInfo title="Destination" content={`${data.name}, ${data.country}`} />}
           </Title>
           <Score>
             <Rating>
-              {data && (
-            <BoxForDestinationInfo title="Rating" content="3.5" />
-            )}
+              {data && <BoxForDestinationInfo title="Rating" content="3.5" />}
             </Rating>
             <Check>
               <CustomCheckbox />
             </Check>    
-          
           </Score>
         </Heading>
         <Description>
-          {data && (
-          <BoxForDestinationInfo title="Short Description" content={data.info} />
-          )}
+          {data && <BoxForDestinationInfo title="Short Description" content={data.info} />}
         </Description>
         <Attraction>
-          {data && (
-          <BoxForDestinationInfo title="Review" content="Review Function Coming Soon" />
-          )}
+          {data && <BoxForDestinationInfo title="Review" content="Review Function Coming Soon" />}
         </Attraction>
-        
       </GridContainer>
-
-      
     </div>
   );
 };
