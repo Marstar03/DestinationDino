18fbe85418353c1b652e7489da1cca6016b2a934
Pelle Traalum Okkenhaug <pelle.okkenhaug@gmail.com>
2024-03-14 01:48:52 +0100

    Every 4th box is now reserved for ads

--- destinationdino/frontend/destinationdino/src/components/DestinationGrid.tsx
+++ destinationdino/frontend/destinationdino/src/components/DestinationGrid.tsx
@@ -6,11 +6,43 @@ interface DestinationGridProps {
 }
 
 const DestinationGrid: React.FC<DestinationGridProps> = ({ destinations }) => {
+  const generateAdBox = () => ({
+    id: null,
+    isAd: true,
+    name: "AD",
+    country: "",
+    picture: null, // add default picture at some point i think would be cool
+    link: "#",
+  });
+
+  const destinationsWithAds = destinations.reduce<
+    (DestinationProps | { isAd: boolean })[]
+  >((acc, dest, index) => {
+    acc.push(dest);
+    if ((index + 1) % 4 === 0) {
+      acc.push(generateAdBox()); // only modulo 4 will be an add
+    }
+    return acc;
+  }, []);
+
+  // I just added this style to make the ad stand out a bit more, didnt want to mess with the original css
+  const adStyle: React.CSSProperties = {
+    fontSize: "4em",
+    textAlign: "center",
+    paddingTop: "69px",
+  };
+
   return (
     <div className="destination-grid">
-      {destinations.map((dest, index) => (
-        <DestinationBox key={index} {...dest} />
-      ))}
+      {destinationsWithAds.map((dest, index) =>
+        dest.isAd ? (
+          <div key={`ad-${index}`} className="destination-box" style={adStyle}>
+            AD
+          </div>
+        ) : (
+          <DestinationBox key={dest.id || index} {...dest} />
+        )
+      )}
     </div>
   );
 };
--- destinationdino/frontend/destinationdino/src/pages/Home.tsx
+++ destinationdino/frontend/destinationdino/src/pages/Home.tsx
@@ -1,23 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import DestinationGrid from '../components/DestinationGrid';
-import { getRequest } from '../httpMethods/getRequest';
+import React, { useState, useEffect } from "react";
+import DestinationGrid from "../components/DestinationGrid";
+import { getRequest } from "../httpMethods/getRequest";
 
 export default function Home() {
-    const apiUrl = 'http://localhost:8080/destinations';
-    const { data: destinations, loading, error } = getRequest(apiUrl);
+  const apiUrl = "http://localhost:8080/destinations";
+  const { data: destinations, loading, error } = getRequest(apiUrl);
 
-    if (loading) {
-        return <div>Loading destinations...</div>;
-    }
+  if (loading) {
+    return <div>Loading destinations...</div>;
+  }
 
-    if (error) {
-        return <div>Error fetching destinations: {error}</div>;
-    }
+  if (error) {
+    return <div>Error fetching destinations: {error}</div>;
+  }
 
-    return (
-        <div>
-            <h2>Destinations</h2>
-            <DestinationGrid destinations={destinations} />
-        </div>
-    );
+  return (
+    <div>
+      <h2>Destinations</h2>
+
+      <DestinationGrid destinations={destinations} />
+    </div>
+  );
 }
